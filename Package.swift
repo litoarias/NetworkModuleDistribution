566c5b4f796ec649a74508f24ee881bc6b4c65a9
litoarias <lito.arias.cervero@gmail.com>
2022-02-03 22:48:36 +0100

    nothing

--- Package.swift
+++ Package.swift
@@ -15,9 +15,9 @@ let package = Package(
             targets: ["Alamofire"]
         )
     ],
-//    dependencies: [
-//        .package(name: "Alamofire", path: "Alamofire.xcframework")
-//    ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.5.0"))
+    ],
     targets: [
         .binaryTarget(
             name: "NetworkModule",
