90b9628830017b1db414ba42910d127fa80a928d
litoarias <lito.arias.cervero@gmail.com>
2022-02-03 22:12:52 +0100

    fix name

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
 //        )
     ],
     dependencies: [
-        .package(path: "../Alamofire")
+        .package(path: "Alamofire")
         // Dependencies declare other packages that this package depends on.
     ],
     targets: [
