a0d815fef6568a6777409eef7c9a25eca8d5107b
litoarias <lito.arias.cervero@gmail.com>
2022-02-03 18:38:55 +0100

    Added package manifest

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.5
+import PackageDescription
+let package = Package(
+    name: "NetworkModule",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "NetworkModule", 
+            targets: ["NetworkModule"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "NetworkModule", 
+            path: "NetworkModule.xcframework")
+    ])
