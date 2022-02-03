478c00ff7810efbc098fcc8276ce62ee24b48f30
litoarias <lito.arias.cervero@gmail.com>
2022-02-03 22:21:48 +0100

    another crazy

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(path: "Alamofire")
+        .package(path: "../Alamofire")
     ],
     targets: [
         .binaryTarget(
