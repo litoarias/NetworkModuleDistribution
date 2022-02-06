6e54652dab032b443d44c6b918b968ab70002559
litoarias <lito.arias.cervero@gmail.com>
2022-02-06 19:48:28 +0100

    update script

--- prepare_package.sh
+++ prepare_package.sh
@@ -22,11 +22,11 @@ echo "replace checksum information in package manifest"
 sed -E -i '' 's/checksum: ".+"/checksum: "'$NEW_CHECKSUM\"/ Package.swift
 echo "print out package manifes for convenience reasons"
 cat Package.swift
-echo "delete downloaded zip file"
-rm $FRAMEWORK_NAME.xcframework.zip
-echo "git commit and push"
-git add . 
+echo "git commit all without framework and push"
+git add --all -- :!$FRAMEWORK_NAME.xcframework.zip
 git commit -m "New $FRAMEWORK_NAME version $NEW_VERSION"
 git push
 echo "Releasing the new version"
-gh release create "$NEW_VERSION" --generate-notes
+gh release create "$NEW_VERSION" --generate-notes "./$FRAMEWORK_NAME.xcframework.zip"
+echo "delete downloaded zip file"
+rm $FRAMEWORK_NAME.xcframework.zip
