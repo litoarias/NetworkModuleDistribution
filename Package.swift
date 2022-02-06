0d59f391d70a0eebe02d40da4281dba8579edfc5
Hipolito Arias <hipolito.arias@kairosds.com>
2022-02-06 18:44:10 +0100

    New NetworkModule version 0.0.2

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = "0.0.1"
+let version = "0.0.2"
 let moduleName = "NetworkModule"
 
 let package = Package(
@@ -21,7 +21,7 @@ let package = Package(
         .binaryTarget(
             name: moduleName,
             url: "https://github.com/litoarias/NetworkModule/releases/download/\(version)/\(moduleName).xcframework.zip",
-            checksum: "d265248d191de61b0bf43270d080b329b3c299bee54c48420948d31abe83ad00"
+            checksum: "bdc0a10c4101b82e74a2e7f6f7e3fa424c8b16c6dd9492ffd6f149ff01e2e4fd"
         )
     ]
 )
