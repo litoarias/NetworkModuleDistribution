9900c25d5b7889cddaa581f0bd1d26cbe2608fff
Hipolito Arias <hipolito.arias@kairosds.com>
2022-02-07 14:09:39 +0100

    New NetworkModule version 0.0.1

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@
 
 import PackageDescription
 
-let version = "1.2.2"
+let version = "0.0.1"
 let moduleName = "NetworkModule"
-let checksum = "8f42f67a0665798e1c4e149c73a36fb9dddc062bc90e3b4e077ea571613ddda1"
+let checksum = "aa6aa56a0d9e46b16c08ae5b580e3d7ba4c3cb3037fa2ba0538c71e86bc96efd"
 
 let package = Package(
     name: moduleName,
